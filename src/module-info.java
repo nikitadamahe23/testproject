9045517147eb1c7da6434d8d65955f9e2bf7ab1f
NIKITA DAMAHE <nikeetadamahe@gmail.com>
2023-02-07 21:27:02 +0530

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lenovo
+ *
+ */
+module testing_eclipdse_togitHub {
+}
---
+++ src/testing_eclipdse_togitHub/a.java
@@ -0,0 +1,10 @@
+package testing_eclipdse_togitHub;
+
+public class a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
